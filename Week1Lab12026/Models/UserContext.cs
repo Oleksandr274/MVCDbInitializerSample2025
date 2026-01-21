b6ba10975856c428dd219921ceaf99d11a89c041
Oleksandr274 <s00274217@atu.ie>
2026-01-21 11:23:17 +0000

    S00274217 Database initialised

---
+++ Week1Lab12026/Models/UserContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Week1Lab12026.Models
+{
+    public class UserContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UserContext()
+            : base()
+        {
+            //Database.EnsureCreated(); // Automatic migrations
+        }
+
+        public UserContext(DbContextOptions options): base(options) 
+        {
+        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+        }
+
+
+    }
+}
--- Week1Lab12026/Program.cs
+++ Week1Lab12026/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 using Tracker.WebAPIClient;
+using Week1Lab12026.Models;
 
 namespace Week1Lab12026
 {
@@ -6,13 +9,39 @@ namespace Week1Lab12026
     {
         public static void Main(string[] args)
         {
+            ActivityAPIClient.Track(
+                StudentID: "S00274217",
+                StudentName: "Oleksandr Keshel",
+                activityName: "Rad302 2026 Week 1 Lab 1",
+                Task: "Database Initializer setup successfully"
+            );
+
             var builder = WebApplication.CreateBuilder(args);
+
+            //retrieve connection string from appsettings.json 
+            var dbConnectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found");
+
+            //create the UserContext with the connection string
+            builder.Services.AddDbContext<UserContext>(options =>
+                //new target assembly directive for migrations
+                options.UseSqlServer(dbConnectionString));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
+            //retrieve user context from the services container
+            using( var scope = app.Services.CreateScope())
+            {
+                var _ctx = scope.ServiceProvider.GetRequiredService<UserContext>();
+                //retrieve the IWebHostEnvironment for the Content Root (even though we are not using the file system here)
+                var hostEnvironment = scope.ServiceProvider.GetRequiredService<IWebHostEnvironment>();
+                // seed data from DbSeeder class
+                DbSeeder dbSeeder = new DbSeeder(_ctx, hostEnvironment);
+                dbSeeder.Seed();
+            }
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
@@ -20,13 +49,7 @@ namespace Week1Lab12026
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            ActivityAPIClient.Track(
-                StudentID: "S00274217", 
-                StudentName: "Oleksandr Keshel", 
-                activityName: "Rad302 2026 Week 1 Lab 1", 
-                Task: "Project Setup "
-            );
+            
 
             app.UseHttpsRedirection();
             app.UseRouting();
